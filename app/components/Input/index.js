cb8221cad5b7e50eb754ff78a1ab9cb1b0fa12da
JEbertPrime <36890230+JEbertPrime@users.noreply.github.com>
2020-08-04 11:40:37 -0400

    initial

---
+++ app/components/Input/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  font-size: 2em;
+  margin-bottom: 0.25em;
+  margin-top:0px;
+`;
+
+export default Input;
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -4,15 +4,40 @@
  * This is the first thing users see of our App, at the '/' route
  *
  */
+//Sagas
+import injectSaga from 'utils/injectSaga';
+import { DAEMON } from 'utils/constants';
+import saga from './saga';
+
+
+
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import GlobalStyle from '../../global-styles';
+import styled from 'styled-components';
+
+//Components imports
+import H1 from 'components/H1';
+import Input from 'components/Input'
+
+const HomePageWrapper = styled.div`
+  max-width: calc(768px + 16px * 2);
+  margin: 0 auto;
+  display: flex;
+  min-height: 100%;
+  padding: 0 16px;
+  flex-direction: column;
+`;
 
 export default function HomePage() {
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+     <HomePageWrapper> 
+        <H1>
+          <FormattedMessage {...messages.header} />
+        </H1>
+          <Input />
+      </HomePageWrapper>
   );
 }
