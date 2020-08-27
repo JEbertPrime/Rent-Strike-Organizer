51d5e62fd0948b7117ca9a6b86e674b141a056db
JEbertPrime <uhsalad@gmail.com>
2020-08-27 17:35:38 -0400

    initial

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost:27017/rent-strike-site',
-    secretOrKey: 'secret'
+  mongoURI: process.ENV.mongoURI,
+  secretOrKey: process.ENV.secretOrKey,
 };
